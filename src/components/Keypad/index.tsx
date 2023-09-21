11bca02b690e7a5473f2fb84f8d27b881d482f26
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-09-21 19:31:33 -0300

    The functionality of the calculator buttons has been added

--- src/components/Keypad/index.tsx
+++ src/components/Keypad/index.tsx
@@ -1,11 +1,70 @@
+"use client";
+
+import { useContext } from "react";
 import s from "./styles.module.scss";
+import { CalcContext } from "@/contexts/CalcContext";
+import { requestCalc } from "@/services/calcApi";
 
 export default function Keypad() {
+  const { expression, setExpression, stage, setStage } =
+    useContext(CalcContext);
+
+  function removeLastChar(): void {
+    setExpression(expression.slice(0, -1));
+
+    if (stage === "") {
+      const regexNum = /(\d{0,}\.?\d{1,})/;
+      const nums = expression.match(regexNum);
+
+      if (nums) {
+        setStage(nums[nums.length - 1]);
+        return;
+      }
+    }
+
+    setStage(stage.slice(0, -1));
+  }
+
+  function onClickClear(): void {
+    setExpression('');
+    setStage('');
+  }
+
+  function insertStringIntoExpression(string: string): void {
+    setExpression(expression + string);
+    setStage(stage + string);
+  }
+
+  function onClickNumber(num: number): void {
+    insertStringIntoExpression(num.toString());
+  }
+
+  function onClickDot(): void {
+    const regexDecimalSeparator = /[\.,]/;
+    if (regexDecimalSeparator.test(stage)) {
+      alert("The number should have only one decimal part.");
+      return;
+    }
+
+    insertStringIntoExpression(".");
+  }
+
+  function onClickOperations(operator: string): void {
+    if (stage === "") return;
+
+    insertStringIntoExpression(operator);
+    setStage("");
+  }
+
+  async function onClickEqual(): Promise<void> {
+    await requestCalc(expression);
+  }
+
   return (
     <>
       <div className={`${s.row} ${s.operations}`}>
-        <button>AC</button>
-        <button>--</button>
+        <button onClick={() => onClickClear()}>AC</button>
+        <button onClick={() => removeLastChar()}>--</button>
         <button>(</button>
         <button>)</button>
       </div>
@@ -13,35 +72,38 @@ export default function Keypad() {
       <div className="flex">
         <div className={s.numericKeypadContainer}>
           <div className={s.row}>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
+            {[7, 8, 9].map((num) => (
+              <button onClick={() => onClickNumber(num)}>{num}</button>
+            ))}
           </div>
 
           <div className={s.row}>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
+            {[4, 5, 6].map((num) => (
+              <button onClick={() => onClickNumber(num)}>{num}</button>
+            ))}
           </div>
 
           <div className={s.row}>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
+            {[1, 2, 3].map((num) => (
+              <button onClick={() => onClickNumber(num)}>{num}</button>
+            ))}
           </div>
 
           <div className={s.row}>
-            <button>.</button>
-            <button>0</button>
-            <button className={s.equalButton}>=</button>
+            <button onClick={onClickDot}>.</button>
+            <button onClick={() => onClickNumber(0)}>{0}</button>
+            <button onClick={onClickEqual} className={s.equalButton}>=</button>
           </div>
         </div>
 
-        <div className={`${s.basicOperations} ${s.operations} ${s.btnNoMargin}`}>
-          <button>/</button>
-          <button>X</button>
-          <button>-</button>
-          <button>+</button>
+        <div
+          className={`${s.basicOperations} ${s.operations} ${s.btnNoMargin}`}
+        >
+          {["/", "*", "-", "+"].map((operator) => (
+            <button onClick={() => onClickOperations(operator)}>
+              {operator == "*" ? "x" : operator}
+            </button>
+          ))}
         </div>
       </div>
     </>
